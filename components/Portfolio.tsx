e362a91a4ba60d3232bee56d1e25171a7f14c4f4
JoseCastillo16 <106782113+JoseCastillo16@users.noreply.github.com>
2025-09-07 18:43:58 -0600

    Cambio

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -162,7 +162,7 @@ const Portfolio = () => {
               </div>
               <div className="absolute -bottom-4 -right-4 bg-blue-600 rounded-full px-4 py-2 flex items-center justify-center">
                 <span className="text-white font-bold text-sm tracking-wider">
-                  {"<JACO>"}
+                  {"JACO"}
                 </span>
 
               </div>
